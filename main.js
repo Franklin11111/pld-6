76fbec2c66ef395e4f656b491f2d1064965255a7
Franklin11111 <abdulovnail6@gmail.com>
2025-07-18 14:56:03 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const nameInput = document.getElementById("name");
+const message = document.getElementById("message");
+// Slides controller
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.querySelectorAll(".slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 };
+    slides[slideIndex - 1].style.display = "flex";
+    setTimeout(showSlides, 3000);
+}
+// Form validation
+//check user entries
+email.addEventListener("input", (e) => {
+    checkInput(email, e);
+})
+nameInput.addEventListener("input", (e) => {
+    checkInput(nameInput, e);
+})
+message.addEventListener("input", (event) => {
+    checkInput(message, event)
+})
+
+form.addEventListener("submit", (event) => {
+    if (!nameInput.validity.valid || !email.validity.valid || !message.validity.valid) {
+        alert("Your data is incomplete");
+        event.preventDefault();
+    } else {
+        alert("Thanks! Your data was submitted successfully.")
+    }
+})
+
+function showError(e) {
+    const el = e.target;
+    if (el.validity.valueMissing) {
+        document.querySelector(`.error-${el.name}`).textContent = `You need to enter your ${el.name}`
+    }
+    if (el.validity.typeMismatch) {
+        document.querySelector(`.error-${el.name}`).textContent = `Entered value needs to be a ${el.name}.`
+    } if (el.validity.patternMismatch) {
+        document.querySelector(`.error-${el.name}`).textContent = `Entered value does not match required pattern of ${el.name}.`
+    }
+    if (el.validity.tooShort) {
+        document.querySelector(`.error-${el.name}`).textContent = `${el.name.charAt(0).toUpperCase() + el.name.slice(1)} length should be at least ${el.minLength} characters long`;
+    }
+}
+
+function checkInput(input, event) {
+    const el = event.target;
+    if (input.validity.valid) {
+        document.querySelector(`.error-${el.name}`).textContent = ""
+    }
+    else {
+        showError(event);
+    }
+}
